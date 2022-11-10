df1120f36591019130d40b04ecfe8b0a1c6ca705
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-11-10 13:46:15 +0900

    ユーザーIDを全てのページでアクセスできる
    ユーザーIDいろんなところで必要だから

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { createContext, useState } from "react";
 import _BaseButton from "../component/atoms/button/_BaseButton";
 
 const CreateUserUrl = 'http://saza.kohga.local:3001/createuser';
+export const UserIdContext = createContext({} as {
+    userId: string
+    setUserId: React.Dispatch<React.SetStateAction<string>>
+})
 
 const WelcomePage: NextPage = () => {
     const router = useRouter();
+    const [userId, setUserId] = useState<string>('')
     const onClickCarUse = async () => {
+        router.push('/CarMenu'); //DEBUG
         try {
             const res = await fetch(CreateUserUrl);
             const data = await res.json();
             if (res.status === 200 && data.succeeded) {
                 const id = data.userId;
                 console.log('userId', id);
+                setUserId(id);
                 router.push('/CarMenu');
             } else {
                 alert('ユーザーIDとれなかったよ')
@@ -23,12 +31,14 @@ const WelcomePage: NextPage = () => {
     }
     return (
         <>
-            <_BaseButton onClick={onClickCarUse} _class='button'>
-                車をつかう
-            </_BaseButton>
-            <_BaseButton _class="button">
-                車管理
-            </_BaseButton>
+            <UserIdContext.Provider value={{ userId, setUserId }}>
+                <_BaseButton onClick={onClickCarUse} _class='button'>
+                    車をつかう
+                </_BaseButton>
+                <_BaseButton _class="button">
+                    車管理
+                </_BaseButton>
+            </UserIdContext.Provider>
         </>
     )
 }
