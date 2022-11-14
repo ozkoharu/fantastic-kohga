b744a583ce5cf65a45f5711b7608f29e58e8c5ec
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-11-15 08:56:25 +0900

    useContextを配置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
-import '../styles/globals.css'
-import "leaflet/dist/leaflet.css"
-import type { AppProps } from 'next/app'
+import '../styles/global.css';
+import '../styles/modal.css';
+import "leaflet/dist/leaflet.css";
+import type { AppProps } from 'next/app';
+import { createContext, useState } from 'react';
+
+
+export const UserIdContext = createContext({} as {
+  userId: string
+  setUserId: React.Dispatch<React.SetStateAction<string>>
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  const [userId, setUserId] = useState<string>('');
+  return (
+    <>
+      <UserIdContext.Provider value={{ userId, setUserId }}>
+        <Component {...pageProps} />
+      </UserIdContext.Provider>
+    </>
+  )
 
+}
 export default MyApp
