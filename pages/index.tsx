1939efc4922f2f704756a6c52d9705cf6f76a66b
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-11-15 08:55:52 +0900

    モーダルコンポーネントいい感じに配置

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { createContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import _BaseButton from "../component/atoms/button/_BaseButton";
+import { useModal } from "../component/hooks/useModal";
+import { UserIdContext } from "./_app";
 
 const CreateUserUrl = 'http://saza.kohga.local:3001/createuser';
-export const UserIdContext = createContext({} as {
-    userId: string
-    setUserId: React.Dispatch<React.SetStateAction<string>>
-})
 
 const WelcomePage: NextPage = () => {
     const router = useRouter();
-    const [userId, setUserId] = useState<string>('')
+    const [isopen, setIsOpne] = useState(true);
+    const { userId, setUserId } = useContext(UserIdContext);
+    const modalRef = useRef<HTMLUListElement>(null);
+    const modal = useModal();
+
+
     const onClickCarUse = async () => {
         //router.push('/CarMenu'); //DEBUG
         try {
@@ -19,8 +22,10 @@ const WelcomePage: NextPage = () => {
             const data = await res.json();
             if (res.status === 200 && data.succeeded) {
                 const id = data.userId;
-                console.log('userId', id);
                 setUserId(id);
+
+                console.log('userId', userId);
+
                 router.push('/CarMenu');
             } else {
                 alert('ユーザーIDとれなかったよ')
@@ -31,11 +36,20 @@ const WelcomePage: NextPage = () => {
     }
     return (
         <>
-            <UserIdContext.Provider value={{ userId, setUserId }}>
+            {
+                modal.content(
+                    <>
+                        <p>モーダル</p>
+                        <button onClick={() => modal.close()}>閉じる</button>
+                    </>
+                )
+            }
+            <UserIdContext.Provider value={{ userId: 'ababaaba', setUserId: setUserId }}>
+
                 <_BaseButton onClick={onClickCarUse} _class='button'>
                     車をつかう
                 </_BaseButton>
-                <_BaseButton _class="button">
+                <_BaseButton _class="button" onClick={() => modal.open()}>
                     車管理
                 </_BaseButton>
             </UserIdContext.Provider>
