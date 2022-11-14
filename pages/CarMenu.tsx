187086a9108039b80ec163c233cef76484582b88
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-11-15 08:57:40 +0900

    ababab

--- pages/CarMenu.tsx
+++ pages/CarMenu.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useContext } from "react";
+
 import _BaseButton from "../component/atoms/button/_BaseButton";
 import { CheckBoxForm } from "../component/atoms/checkbox/checkBoxForm";
+import { UserIdContext } from "./_app";
 
 const CarMenu: NextPage = () => {
     const router = useRouter();
+    const { userId } = useContext(UserIdContext);
     const onClickDesitination = () => {
         router.push('/Desitination');
     }
@@ -20,7 +24,7 @@ const CarMenu: NextPage = () => {
     const onClickJunkai = () => {
 
     }
-
+    console.log('CarMenu_userId', userId);
     return (
         <>
             <_BaseButton onClick={onClickDesitination} _class="button">
