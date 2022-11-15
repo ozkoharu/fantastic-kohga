2e980da69876ac76d4e3ad97063f38dfee4f5cc9
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-11-15 14:08:28 +0900

    終わりページwip

--- pages/EndPage.tsx
+++ pages/EndPage.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 const EndPage: NextPage = () => {
+
     return (
         <>
             <h1>終わり</h1>
+            <Link href='/'>HOME</Link>
         </>
     )
 }
